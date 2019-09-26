2ae24594405c2c95b9491a853402358ebb404071
Collin Lilienthal <collinlilienthal@dhcp83-50.calvin.edu>
2019-09-26 17:32:06 -0400

    Homework01

---
+++ Homework01/src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  firstNumber;
+  secondNumber;
+  result;
+
+    add() {
+      this.result = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+    }
+    sub() {
+      this.result = parseInt(this.firstNumber) - parseInt(this.secondNumber);
+    }
+    mult() {
+      this.result = parseInt(this.firstNumber) * parseInt(this.secondNumber);
+    }
+    div() {
+      this.result = parseInt(this.firstNumber) / parseInt(this.secondNumber);
+    }
+  }
+
